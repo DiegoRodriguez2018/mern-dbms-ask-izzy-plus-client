98bb4c6488cd3e76758f2306013b0c0033643d92
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2019-05-21 06:27:57 +1000

    Added node-sass package as a dependency to directly import .scss files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
-import './stylesheets/App.css';
+import './stylesheets/App.scss';
 import history from './history';
 import Register from './Components/Register';
 import Home from './Components/Home';
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import GoogleLogin from "react-google-login";
-import history from "../history";
+import React, { Component } from 'react';
+import GoogleLogin from 'react-google-login';
+import history from '../history';
 
 class Login extends Component {
   responseGoogle = response => {
-    localStorage.setItem("token", response.tokenId);
-    history.push("/dashboard");
+    localStorage.setItem('token', response.tokenId);
+    history.push('/dashboard');
   };
 
   errorGoogle = response => {
@@ -13,36 +13,44 @@ class Login extends Component {
   };
 
   handleGoogleLogin = () => {
-    console.log("Successfully clicked!");
+    console.log('Successfully clicked!');
   };
 
   redirect = () => {
-    history.push('/admin')
-  }
+    history.push('/admin');
+  };
+
+  handleGuestLogin = () => {
+    const guestData = {
+      email:guest_user
+    }
+    localStorage.setItem('token', guestData);
+    history.push('/dashboard');
+  };
 
   render() {
     const googleClient = process.env.REACT_APP_GOOGLE_CLIENT_ID;
     return (
-      <div className="login-button-container">
-
+      <div className='login-button-container'>
         <GoogleLogin
           clientId={googleClient}
           render={renderProps => (
-            <button onClick={renderProps.onClick} className="login-button">Login</button>
+            <button onClick={renderProps.onClick} className='login-button'>
+              Login
+            </button>
           )}
-          buttonText="Login"
+          buttonText='Login'
           onSuccess={this.responseGoogle}
           onFailure={this.errorGoogle}
-          className="login-button"
+          className='login-button'
         />
-
-        <div className="toggle-login-container">
-        
-          <a className="toggle-login" onClick={this.redirect} >
-          Are you an Admin?
+        <div className='toggle-login-container'>
+          <a className='toggle-login' onClick={this.redirect}>
+            Are you an Admin?
           </a>
-          
-        <a className="guest-button">Try as Guest</a>
+          <a className='guest-button' onClick={this.handleGuestLogin}>
+            Try it as a Guest
+          </a>
         </div>
       </div>
     );
